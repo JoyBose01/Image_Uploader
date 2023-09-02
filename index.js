ae3af165950bdf192a1ab38af70146bc0efaac27
Joy-bose11 <github.joy@gmail.com>
2023-09-02 11:08:28 +0600

    server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const colors = require("colors");
-const uploader = require("../middleware/uploader");
+const uploader = require("./middleware/uploader");
 
 //middlewares
 app.use(express.json());
